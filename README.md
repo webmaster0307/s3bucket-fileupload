cf7e29fc8324a908f2018e16a2754fa6adecd7d6
dipeshs <dipesh357@hotmail.com>
2016-05-24 20:31:25 +0530

    The 1st Commit with all files...

---
+++ README.md
@@ -0,0 +1,28 @@
+Image Upload to Amazon S3 Bucket from Mobile Devices Cross Platform.
+- Using NodeJS
+
+
+
+Titanium Image Upload Code
+
+var imageuploadxhr = Titanium.Network.createHTTPClient();
+var response;    
+
+imageuploadxhr.onload = function(e) {
+	//Ti.API.info(this.responseText);
+	response=JSON.parse(this.responseText);
+	url=response.file;  //image url returned from server
+};
+imageuploadxhr.onerror = function(e) {
+	Ti.API.info("error > "+JSON.stringify(e));
+};
+imageuploadxhr.open("POST", "http://192.168.2.179:5600?type=type_of_plugin&imagename=" + fileName + ".jpg"); //Dipesh's IPAddress
+imageuploadxhr.send(iImage); // iImage is image Object 
+
+
+
+config.json has all configuration for s3 Bucket
+and also the AWS Configuration
+
+
+config also has the port on which your app must run.
---
+++ app.js
@@ -0,0 +1,69 @@
+var AWS = require('aws-sdk');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var _ = require('underscore');
+var fs = require('fs');
+var zlib = require('zlib');
+var awsConfig = require('./config.json');
+
+// console.dir(awsConfig.aws);
+// console.dir(awsConfig.s3);
+
+AWS.config.update(awsConfig.aws); // the configuration in config.json File
+
+var params = awsConfig.s3; // s3 bucket configuration
+
+var return_image_path = "https://s3-us-west-2.amazonaws.com/myimagebucket/"; // change this as this url will be send to your mobile device stating that the image is uploaded at this URL
+
+
+http.createServer(function(request,response){
+var s3 = new AWS.S3();
+  var url_parts = url.parse(request.url, true);
+  var query = url_parts.query;
+  // console.log("hot server"+JSON.stringify(query)); 
+  // data : ----------------> "type":"mytype","imagename":"7208079951_201652417469.jpg"
+    var tempFile = fs.createWriteStream(__dirname+"/"+query.imagename);
+    request.pipe(tempFile);
+
+    var fileSize = request.headers['content-length'];
+    var uploadedBytes = 0 ;
+
+    request.on('data',function(d){
+      uploadedBytes += d.length;
+      var progress = (uploadedBytes/fileSize) * 100;
+      console.log(progress); // displays the percentage as how much percentage image is uploaded //response.write(progress);
+    });
+
+    request.on('end',function(){
+      var respdata = {
+        "file":profile_path+"prescriptions/"+query.imagename,
+        "imagename":query.imagename
+      };
+      console.log("resp > "+JSON.stringify(respdata));
+      var body = fs.createReadStream(query.imagename);
+      params.Key = query.imagename;
+      params.Body = body;
+      
+      // params.Bucket = params.Bucket+"/my_folder";     
+      // uncomment if your bucket has a folder and you want to upload in that folder instead of the bucket directly.
+
+      s3.upload(params, function(err, data) {
+        if(!err){
+          // console.dir(data); -- shows whether the image is uploaded or not.
+          fs.unlink(query.imagename, (err) => {
+            if (err) throw err;
+            console.log('successfully deleted tempFile'); // delete the file from localmachine after uploading image to s3 bucket
+          });
+        }else{
+          console.dir(err); // shows the error
+        }
+      });
+
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.write(JSON.stringify(respdata));
+      response.end();
+    });
+}).listen(awsConfig.port,function(){
+  console.log("server started at %d",awsConfig.port);
+});
